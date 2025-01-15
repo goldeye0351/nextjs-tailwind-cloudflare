0a14af5dffc0df6ce6559bde2d768f5d280ea8e2
Goldeye0351 <goldeye0351@gmail.com>
2025-01-15 19:58:43 +0800

    Update route.ts

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,11 +1,3 @@
-import { NewsletterAPI } from 'pliny/newsletter'
-import siteMetadata from '@/data/siteMetadata'
-
-//export const dynamic = 'force-static'
-export const runtime = 'edge';
-const handler = NewsletterAPI({
-  // @ts-ignore
-  provider: siteMetadata.newsletter.provider,
-})
-
-export { handler as GET, handler as POST }
+export async function GET() {
+  return new Response('Hello, World!')
+}
